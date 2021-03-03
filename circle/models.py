c9ff7a8ee78d149fe4f2abf25f14dc7bc91ea7f5
juchilian <juchilian@outlook.jp>
2021-03-03 10:42:59 +0900

    詳細ページ完了

--- circle/models.py
+++ circle/models.py
@@ -5,7 +5,7 @@ User = settings.AUTH_USER_MODEL
 
 class Circle(models.Model):
     owner = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
-    image = models.ImageField(upload_to='image/', blank=True, null=True)
+    image = models.ImageField(upload_to='media/image/', blank=True, null=True)
     name =  models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True, max_length=400)
     twitter_url = models.URLField(null=True, blank=True)
--- circle/views.py
+++ circle/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Circle
 # Create your views here.
 
@@ -8,8 +8,11 @@ def circle_list_view(request):
     template_name = 'circle/list.html'
     return render(request, template_name, context)
     
-def circle_list_view(request):
-    circles = Circle.objects.all()
-    context = {"title": "サークル一覧" ,"circles": circles}
-    template_name = 'circle/list.html'
+def circle_detail_view(request, circle_id):
+    circle = get_object_or_404(Circle, id=circle_id)
+    is_owner = False
+    if circle.owner == request.user:
+        is_owner = True
+    context = {"title": "サークル詳細" ,"circle": circle}
+    template_name = 'circle/detail.html'
     return render(request, template_name, context)
