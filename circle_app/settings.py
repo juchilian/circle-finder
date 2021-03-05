12dbd0d85c0887cecd42908bf142317d98dc0b3f
juchilian <juchilian@outlook.jp>
2021-03-05 10:58:59 +0900

    aws S3の設定完了 adminのいくつかのファイルに403 (Forbidden)

--- circle_app/settings.py
+++ circle_app/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'bootstrap4',
+    'storages',
     # My apps
     'circle',
 ]
@@ -143,3 +144,11 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY') 
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = True
+AWS_DEFAULT_ACL = None # the file will inherit the bucket’s permission
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
