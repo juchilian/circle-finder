743b659d546d3ab3a249a935f5a38f261fa82c0c
juchilian <juchilian@outlook.jp>
2021-03-07 09:57:57 +0900

    like機能完成 without ajax

--- circle/urls.py
+++ circle/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import circle_list_view,circle_detail_view, circle_edit_view
+from .views import circle_list_view,circle_detail_view, circle_edit_view, circle_like_unlike
 
+app_name = 'circle'
 urlpatterns = [
-    path('', circle_list_view),
-    path('<slug:slug>/', circle_detail_view),
+    path('', circle_list_view, name='main-circle-view'),
+    path('liked/', circle_like_unlike, name='like-circle-view'),
+    path('<slug:slug>/', circle_detail_view, name='detail-circle-view'),
     path('<slug:slug>/edit/', circle_edit_view),
 ]
