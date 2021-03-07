bc442e371aeba6a5c50de9ad2bd0ccd7f8e3add6
juchilian <juchilian@outlook.jp>
2021-03-07 18:47:22 +0900

    サークル代表用フォーム編集

--- circle/forms.py
+++ circle/forms.py
@@ -8,9 +8,21 @@ class CircleForm(forms.Form):
 
 
 class CircleModelForm(forms.ModelForm):
+    practice_date = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '例) 平日毎日、月～水 etc...'
+    }))
+    practice_place = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '例) 小机、多摩川 etc...'
+    }))
+    twitter_url = forms.URLField(widget=forms.TextInput(attrs={
+        'placeholder': '例) https://twitter.com/keiotennis'
+    }))
+    insta_url = forms.URLField(widget=forms.TextInput(attrs={
+        'placeholder': '例) https://www.instagram.com/keiotennis'
+    }))
     class Meta:
         model = Circle
-        fields = ['image','description', 'twitter_url', 'insta_url']
+        fields = ['image','description', 'budget', 'members_num', 'gender_rate', 'alcohol', 'hard', 'practice_date', 'practice_place','twitter_url', 'insta_url']
     
     # def clean_title(self, *args, **kwargs):
     #     instance = self.instance
