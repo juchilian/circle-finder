f045a50bcccdf9fdc06145d3a4b5535c9227655e
juchilian <juchilian@outlook.jp>
2021-03-16 12:21:25 +0900

    forms.py required=False追加

--- circle/forms.py
+++ circle/forms.py
@@ -1,26 +1,21 @@
 from django import forms
 
 from .models import Circle
-class CircleForm(forms.Form):
-    title = forms.CharField()
-    slug = forms.SlugField()
-    content = forms.CharField(widget=forms.Textarea)
-
 
 class CircleModelForm(forms.ModelForm):
-    practice_date = forms.CharField(label='活動日',widget=forms.TextInput(attrs={
+    practice_date = forms.CharField(required=False,label='活動日',widget=forms.TextInput(attrs={
         'placeholder': '例) 平日毎日、月～水 etc...'
     }))
-    practice_place = forms.CharField(label='活動場所',widget=forms.TextInput(attrs={
+    practice_place = forms.CharField(required=False,label='活動場所',widget=forms.TextInput(attrs={
         'placeholder': '例) 小机、多摩川 etc...'
     }))
-    twitter_url = forms.URLField(widget=forms.TextInput(attrs={
+    twitter_url = forms.URLField(required=False,widget=forms.URLInput(attrs={
         'placeholder': '例) https://twitter.com/keiotennis'
     }))
-    insta_url = forms.URLField(widget=forms.TextInput(attrs={
+    insta_url = forms.URLField(required=False,widget=forms.URLInput(attrs={
         'placeholder': '例) https://www.instagram.com/keiotennis'
     }))
-    line_url = forms.URLField(widget=forms.TextInput(attrs={
+    line_url = forms.URLField(required=False,widget=forms.URLInput(attrs={
         'placeholder': '例) https://line.me/keiotennis'
     }))
     class Meta:
