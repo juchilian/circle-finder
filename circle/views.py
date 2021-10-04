090fddc14135901d97997b766eaffdedc95dd327
juchilian <juchilian1227@gmail.com>
2021-10-04 10:35:15 +0900

    [update] コメント整理

--- circle/views.py
+++ circle/views.py
@@ -9,6 +9,7 @@ from .models import Circle, Like, HARD_LIST, ALCOHOL_LIST, EXPERIENCED_LIST, EVE
 from .forms import CircleModelForm
 # Create your views here.
 
+
 def circle_list_view(request):
     circles = Circle.objects.all()
     paginator = Paginator(circles, 4)
@@ -19,7 +20,7 @@ def circle_list_view(request):
         next_url = f'?page={page.next_page_number()}'
     else:
         next_url = ''
-    
+
     if page.has_previous():
         prev_url = f'?page={page.previous_page_number()}'
     else:
@@ -30,25 +31,29 @@ def circle_list_view(request):
         "page": page,
         "next_page_url": next_url,
         "prev_page_url": prev_url,
-        }
+    }
     template_name = 'circle/list.html'
     return render(request, template_name, context)
-    
+
+
 def circle_detail_view(request, slug):
     circle = get_object_or_404(Circle, slug=slug)
-    context = {"title": "サークル詳細" ,"circle": circle}
+    context = {"title": "サークル詳細", "circle": circle}
     template_name = 'circle/detail.html'
     return render(request, template_name, context)
+
 
 @staff_member_required
 def circle_edit_view(request, slug):
     circle = get_object_or_404(Circle, slug=slug)
     if circle.owner == request.user:
+        # リクエストがPOSTならrequest.POST, GETならNone
         form = CircleModelForm(request.POST or None, instance=circle)
-        if form.is_valid():
+        if form.is_valid():  # リクエストPOSTで正しい入力だった場合
             form.save()
             messages.success(request, "正常に更新されました！")
             return redirect("/")
+        # GETの場合は編集画面表示
         context = {"title": "サークル情報の編集", 'form': form}
         template_name = 'circle/form.html'
         return render(request, template_name, context)
@@ -56,49 +61,46 @@ def circle_edit_view(request, slug):
         # 権限がない人によるアクセス
         raise Http404
 
+
 @login_required
 def circle_like_unlike(request):
-    print(request.method)
     user = request.user
-    if request.method == 'POST':
-        circle_id = request.POST.get('circle_id')
-        circle_obj = Circle.objects.get(id=circle_id)
-
-        if user in circle_obj.liked.all():
-            circle_obj.liked.remove(user)
-        else:
-            circle_obj.liked.add(user)
-        
-        like, created = Like.objects.get_or_create(user=user, circle_id=circle_id)
-
-        if not created:
-            if like.value=='Like':
-                like.value='Unlike'
-            else:
-                like.value='Like'
-        else:
-            like.value='Like'
-
-        circle_obj.save()
-        like.save()
-        data = {
-            'value': like.value,
-        }
-        print('Json', data)
-        return JsonResponse(data, safe=False)
-    # next_url = request.GET.get()
     circle_id = request.POST.get('circle_id')
     circle_obj = Circle.objects.get(id=circle_id)
-    url = circle_obj.slug
-    return redirect(f'/circle/{url}')
+
+    if user in circle_obj.liked.all():
+        # サークルのlikedカラムにuserが含まれていたら
+        circle_obj.liked.remove(user)
+    else:
+        circle_obj.liked.add(user)
+
+    like, created = Like.objects.get_or_create(
+        user=user, circle_id=circle_id)
+
+    if not created:  # レコードが作られなかった場合
+        if like.value == 'Like':
+            like.value = 'Unlike'
+        else:
+            like.value = 'Like'
+    else:
+        like.value = 'Like'
+
+    circle_obj.save()
+    like.save()
+    data = {
+        'value': like.value,
+    }
+    return JsonResponse(data, safe=False)
+
 
 def circle_search_view(request):
     hard = request.GET.get('hard')
     experienced = request.GET.get('experienced')
     alcohol = request.GET.get('alcohol')
-    searched_circles = Circle.objects.annotate(score=Func(F('hard') - hard, 2, function='Power')+Func(F('experienced') - experienced, 2, function='Power')+Func(F('alcohol') - alcohol, 2, function='Power')).order_by('score')[:3]
+    # サークル一つ一つに対してscore計算->上位3つをsearched_circlesに格納
+    searched_circles = Circle.objects.annotate(score=Func(F('hard') - hard, 2, function='Power')+Func(
+        F('experienced') - experienced, 2, function='Power')+Func(F('alcohol') - alcohol, 2, function='Power')).order_by('score')[:3]
     context = {"title": "最適サークル検索結果", 'circles': searched_circles}
     context["params"] = {HARD_LIST, ALCOHOL_LIST, EXPERIENCED_LIST, EVENT_LIST}
     template_name = 'circle/searched.html'
     return render(request, template_name, context)
-        
